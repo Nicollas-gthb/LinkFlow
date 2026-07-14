86a2c60619294e947c60af229e88c70b8239026f
Nicollas-gthb <nicollasoliver063@gmail.com>
2026-07-14 08:08:51 -0300

    fix: correção na anotação web security

--- backend/src/main/java/com/linkflow/config/SecurityConfig.java
+++ backend/src/main/java/com/linkflow/config/SecurityConfig.java
@@ -3,8 +3,8 @@ package com.linkflow.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,7 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableAspectJAutoProxy
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
